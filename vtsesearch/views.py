00c6fe5d5d1217142309f0b1a62afe63509181b6
vibhorai <chander.1@iitj.ac.in>
2022-10-16 23:25:01 +0530

    Added Pagination & Similar word search

--- vtsesearch/views.py
+++ vtsesearch/views.py
@@ -1,5 +1,8 @@
+from gettext import translation
 from django.http import HttpResponse
 from django.shortcuts import render
+
+
 from .documents import CarDocument,VedicTextDocument
 from elasticsearch_dsl import Q
 from elasticsearch.helpers import bulk
@@ -7,33 +10,59 @@ from elasticsearch import Elasticsearch
 from .models import Car,VedicText
 from .search import BGIndex
 from . import models
+from django.core.paginator import Paginator
+
 
 # Create your views here.
 
 
 def search(request):
     searchText = request.GET.get('searchText','default')
-    print('I am here')
-    #car = Car(name="Car one",color="red",type=1,description="A beautiful car")
-    #car.save()
-    #s = CarDocument.search().filter("term", color="red")
-    #vt = VedicText(title="Car one",url="red",translation="A beautiful car")
-    #vt.save()
-    #s = VedicTextDocument.search().filter('term', title="Bhagavad Geeta chapter 1 shloka 1")
-    #s = BGIndex.search()
-    #es = Elasticsearch()
-    #bulk(client=es, actions=(b.indexing() for b in models.VedicText.objects.all().iterator()))
-    query = 'Bhagavad Geeta chapter 1 shloka 1'
+    #q = Q(
+    # 'match_phrase',
+    # translation=searchText)
     q = Q(
-     'match_phrase',
-     title=searchText)
-    search = VedicTextDocument.search().query(q)
-    #response = search.execute()
+     'multi_match',
+     query=searchText,
+     fields=[
+         'title',
+         'translation'
+     ],
+     fuzziness= "AUTO")
+    
+    
+    
+    
+    page_number = int(request.GET.get('page','1'))
+    
+    start = (page_number-1) * 10
+    end = start + 10
+    
+    search = VedicTextDocument.search().query(q)[start:end]
+   
+    
+    
+    
+    p = Paginator(search, 10)
+    
+    
+    try:
+        page_obj = p.get_page(page_number) 
+        
+        
+    except PageNotAnInteger:
+        
+        page_obj = p.page(1)
 
-# print all the hits
-    #for hit in search:
-    #    print("Title : {}, Translation {}".format(hit.title, hit.translation))
+        
+        
+    except EmptyPage:
+        
+        page_obj = p.page(p.num_pages)
+        
         
     
-    params = {'searchedText': search}
-    return render(request, 'searchResults.html',params)
+    context = {'page_obj': page_obj, 'searchText':searchText}
+    
+    
+    return render(request, 'searchResults.html',context)
